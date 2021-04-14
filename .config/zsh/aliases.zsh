128c9f452634f90e6e8412b7553a3759b591d634
ConorHK <conorknowles@gmail.com>
2021-04-14 21:59:49 +0100

    update laptop

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,9 +10,4 @@ alias gs="git status"
 alias gc="git commit"
 alias ga="git add"
 alias gaa="git add --all"
-
-vs () {
-  session=${1:-'Session'}
-  suffix='.vim'
-  vim -S $SESSIONS/$session$suffix
-}
+alias webcam_pic="mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
