c5b073f8c1a60e32e33b52d4522ccbb18d91e550
Conor Knowles <ck@conorknowles.com>
2021-08-07 15:04:42 +0100

    pc fixes

--- .config/zsh/.zsh_plugins.sh
+++ .config/zsh/.zsh_plugins.sh
@@ -4,3 +4,5 @@ source /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zs
 fpath+=( /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
 source /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
 fpath+=( /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt )
+source /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( /home/conor/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
